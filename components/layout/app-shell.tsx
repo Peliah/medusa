402ba9b0b53d2bd3dfbdf6e869590a9ebbb0af03
Peliah <pelepoupa@gmail.com>
2026-05-31 08:41:04 +0100

    feat: add sidebar drawer for viewports below lg

--- components/layout/app-shell.tsx
+++ components/layout/app-shell.tsx
@@ -1,6 +1,7 @@
 import { BuilderHeader } from "@/components/layout/builder-header"
 import { BuilderMain } from "@/components/layout/builder-main"
 import { BuilderResultsDrawer } from "@/components/layout/builder-results-drawer"
+import { BuilderSidebarDrawer } from "@/components/sidebar/builder-sidebar-drawer"
 
 export function AppShell() {
   return (
@@ -12,6 +13,7 @@ export function AppShell() {
       </div>
 
       <BuilderResultsDrawer />
+      <BuilderSidebarDrawer />
     </div>
   )
 }
